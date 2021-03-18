b1291581dc8a3a5d67864bc2c551e5eca63eff36
realfaid <realfaid@gmail.com>
2021-03-18 18:29:09 +0100

    navbar

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function index()
+	public function hlavni()
 	{
-		return view('welcome_message');
+		echo view("head");
 	}
 }
